50ff6951b6e97f237c87fb6d2cafdcd68a175171
Thota Saikumar <134082542+Mazie0@users.noreply.github.com>
2024-03-26 12:36:54 +0530

    Update 1653_AI-ML-AS7.py

--- 1653_AI-ML-AS7.py
+++ 1653_AI-ML-AS7.py
@@ -3,25 +3,24 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-# a) Read the data and find features and target variables
+
 data = pd.read_csv('breast_cancer_survival.csv')
 
 features = data.drop('Patient_Status', axis=1)
 target = data['Patient_Status']
 
 
-# b) Find Target variable (already assumed)
+
 print("Target Variable:", target.name)
 
 
-# c) Train KNN
+
 X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.25, random_state=42)
 
-knn = KNeighborsClassifier(n_neighbors=5)  # Start with K=5
+knn = KNeighborsClassifier(n_neighbors=5)  
 knn.fit(X_train, y_train)
 
 
-# d) Find accuracy for different K values and plot
 k_values = range(2, 21)
 accuracies = []
 
